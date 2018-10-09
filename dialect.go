380a6bd9f6c7399d6c763c831cc979760ccc6190
aihui zhu <mr.zhuah@gmail.com>
2018-10-09 23:09:23 +0800

    add mysql dialect

--- dialect.go
+++ dialect.go
@@ -28,7 +28,7 @@ func (p Postgres) Type(typ, precision, val string) (dbtyp, defval string, err er
 	case "bool":
 		return "BOOLEAN", p.defaultVal("false", val, false), nil
 	case "int":
-		return "INTEGER", p.defaultVal("0", val, false), nil
+		return "BIGINT", p.defaultVal("0", val, false), nil
 	case "int8":
 		return "SMALLINT", p.defaultVal("0", val, false), nil
 	case "int16":
@@ -148,4 +148,103 @@ func (s SQLite3) Type(typ, precision, val string) (dbtyp, defval string, err err
 	default:
 		return "", "", fmt.Errorf("sqlite3: unsupported type: %s", typ)
 	}
+}
+
+type MySQL struct{}
+
+func (MySQL) defaultVal(def, val string, quote bool) string {
+	if val == "" {
+		val = def
+	}
+	if quote {
+		if strings.Contains(val, "'") {
+			val = strings.Replace(val, "\\", "\\\\'", -1)
+			val = strings.Replace(val, "'", "\\'", -1)
+			val = `E'` + val + `'`
+		} else {
+			val = `'` + val + `'`
+		}
+	}
+	return val
+}
+
+func (m MySQL) Type(typ, precision, val string) (dbtyp, defval string, err error) {
+	switch typ {
+	case "bool":
+		return "BOOLEAN", m.defaultVal("false", val, false), nil
+	case "int":
+		return "BIGINT", m.defaultVal("0", val, false), nil
+	case "int8":
+		return "TINYINT", m.defaultVal("0", val, false), nil
+	case "int16":
+		return "SMALLINT", m.defaultVal("0", val, false), nil
+	case "int32":
+		return "INT", m.defaultVal("0", val, false), nil
+	case "int64":
+		return "BIGINT", m.defaultVal("0", val, false), nil
+	case "uint":
+		return "BIGINT UNSIGNED", m.defaultVal("0", val, false), nil
+	case "uint8":
+		return "TINYINT UNSIGNED", m.defaultVal("0", val, false), nil
+	case "uint16":
+		return "SMALLINT UNSIGNED", m.defaultVal("0", val, false), nil
+	case "uint32":
+		return "INT UNSIGNED", m.defaultVal("0", val, false), nil
+	case "uint64":
+		return "BIGINT UNSIGNED", m.defaultVal("0", val, false), nil
+	case "float32", "float64", "float":
+		if precision == "" {
+			if typ == "float32" {
+				precision = "32,4"
+			} else {
+				precision = "64,4"
+			}
+		}
+		if typ == "float32" {
+			typ = "FLOAT"
+		} else {
+			typ = "DOUBLE"
+		}
+		return fmt.Sprintf("%s(%s)", typ, precision), m.defaultVal("0", val, false), nil
+	case "string":
+		if precision == "" {
+			precision = "64"
+		}
+		return fmt.Sprintf("VARCHAR(%s)", precision), m.defaultVal("", val, true), nil
+	case "char":
+		if precision == "" {
+			precision = "64"
+		}
+		return fmt.Sprintf("CHAR(%s)", precision), m.defaultVal("", val, true), nil
+	case "text":
+		return "MEDIUMTEXT", m.defaultVal("", val, true), nil
+	case "blob":
+		return "MEDIUMBLOB", m.defaultVal(``, val, false), nil
+	default:
+		return "", "", fmt.Errorf("postgres: unsupported type: %s", typ)
+	}
+}
+
+func (MySQL) DSN(config DBConfig) string {
+	if config.Host == "" {
+		config.Host = "localhost"
+	}
+	if config.Port == 0 {
+		config.Port = 3306
+	}
+	userPass := config.User
+	if userPass != "" {
+		if config.Password != "" {
+			userPass += ":" + config.Password
+		}
+		userPass += "@"
+	}
+	return fmt.Sprintf(
+		"%s/tcp(%s:%d)/%s?%s",
+		userPass,
+		config.Host,
+		config.Port,
+		config.DBName,
+		config.JoinOptions("=", "&"),
+	)
 }
